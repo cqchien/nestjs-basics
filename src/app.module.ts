23cdda8d2fe046f87b59a465d257dde11ac2c84d
Chiến <caochientp1@gmail.com>
2020-11-02 23:48:47 +0700

    Create User module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './config';
 import { ItemsModule } from './items/items.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ItemsModule, MongooseModule.forRoot(config.mongoURL)],
+  imports: [ItemsModule, UserModule, MongooseModule.forRoot(config.mongoURL)],
   controllers: [AppController],
   providers: [AppService],
 })
